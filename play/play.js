0b06ecdf985ae36439a6e2dbf82f449bbd3f24b0
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-25 16:54:52 -0400

    'cntrl+c' worked!

--- play/play.js
+++ play/play.js
@@ -10,3 +10,22 @@ conn.on('connect', () => {
 //   conn.write(`Move: Up`);
 //   conn.write(`Move: Right`);
 // });
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const stdin = setupInput();
+
+const handleUserInput = function (key) {
+  console.log(`handleUserInput ${key}`);
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+stdin.on("data", (key) => handleUserInput(key));
