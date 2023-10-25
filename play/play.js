08d2788b6a463e3886e2c592bf8ab9eaf5048bae
Urvish Prajapati <urvishprajapati412@gmail.com>
2023-10-25 16:17:10 -0400

    connection module

--- play/play.js
+++ play/play.js
@@ -1,21 +1,7 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
-    port: '50541'           // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (data) => {
-    console.log(`Connected! ${data}`);
-  });
-
-
-  return conn;
-};
+const { connect, net } = require('../client');
 
 console.log("Connecting ...");
-connect();
+const conn = connect();
+conn.on('connect', () => {
+  console.log(`In!`);
+});
